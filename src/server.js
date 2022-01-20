9b1a95bc56d876799783326c7acdced610b4d8b1
Sanjeet Kumar Sangam <93376461+sanjeetSangam@users.noreply.github.com>
2022-01-20 13:06:08 +0530

    ikeaCloneServer

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 const app = require("./index");
 const server = require("./configs/db");
 
+
+
 app.listen(process.env.PORT || 5000, async (req, res) => {
   server();
   console.log("Default port is active on PORT");
